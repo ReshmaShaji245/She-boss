d89724a0f23b37950d0f79c6b30564aa12dd5dcc
Ayesha Khalid <khalid34@mcmaster.com>
2020-06-21 05:34:18 -0400

    categories

--- app.py
+++ app.py
@@ -76,11 +76,24 @@ def signout():
 @app.route("/business")
 def business():
     business=[['Atla', '372 Lafayette St', 'Daniela Soto-Innes', 'https://www.atlanyc.com/', 'Food'], ['Atoboy and Atomix', '43 E. 28th St, New York, NY', 'Ellia Park', 'https://www.atomixnyc.com/ ', 'Food'], ['Ayada', '7708 Woodside Ave, New York, NY', 'Duanjai Thammasat', 'https://ayadathai.com/', 'Food'], ['Baoburg', '614 Manhattan Ave Brooklyn', 'Suchanan Aksornnan', 'http://www.baoburg.com/ ', 'Food'], ['CAP Beauty', '238 West 10 St, New York"', 'Chloe Kernaghan', 'https://www.capbeauty.com/', 'Beauty'], ['The Sill', '84 Hester Street, New York, NY', 'Eliza Blank', 'https://www.thesill.com/ ', 'Home décor'], ['McNally Jackson', '52 Prince St, New York, NY', 'Sarah McNally', 'https://www.mcnallyjackson.com/', 'Books'], ['Frankie', '100 Stanton St , New York, NY', 'Gaelle Drevet', 'https://thefrankieshop.com/', 'Clothes + Home décor '], ['Maryam Nassir Zadeh', '123 Norfolk St, New York, NY', 'Maryam Nassir Zadeh', 'https://mnzstore.com/', 'Clothes']]
-    return render_template("index.html", business)
+    return render_template("homepage.html", business)
 
 @app.route("/categories", methods=["GET", "POST"])
 def categories():
    # names = request.form.get("category")
     names=request.form.getlist("category")
     print(names)
-    return render_template('temp.html')
+    business=[['Atla', '372 Lafayette St', 'Daniela Soto-Innes', 'https://www.atlanyc.com/', 'Food-and-Drink'], ['Atoboy and Atomix', '43 E. 28th St, New York, NY', 'Ellia Park', 'https://www.atomixnyc.com/ ', 'Food-and-Drink'], ['Ayada', '7708 Woodside Ave, New York, NY', 'Duanjai Thammasat', 'https://ayadathai.com/', 'Food-and-Drink'], ['Baoburg', '614 Manhattan Ave Brooklyn', 'Suchanan Aksornnan', 'http://www.baoburg.com/ ', 'Food-and-Drink'], ['CAP Beauty', '238 West 10 St, New York"', 'Chloe Kernaghan', 'https://www.capbeauty.com/', 'Hair-and-Beauty'], ['The Sill', '84 Hester Street, New York, NY', 'Eliza Blank', 'https://www.thesill.com/ ', 'Home Décor'], ['McNally Jackson', '52 Prince St, New York, NY', 'Sarah McNally', 'https://www.mcnallyjackson.com/', 'Books'], ['Frankie', '100 Stanton St , New York, NY', 'Gaelle Drevet', 'https://thefrankieshop.com/', ('Retail','Home Décor')], ['Maryam Nassir Zadeh', '123 Norfolk St, New York, NY', 'Maryam Nassir Zadeh', 'https://mnzstore.com/', 'Retail']]
+    if len(names)==0:
+        return render_template("homepage.html", business= business)
+    else:
+        z=[]
+        for x in range(0,len(names)):
+            for i in business:
+                print(z)
+                print(x)
+                if names[x] in i[4]:
+                    z+=[i]
+                    
+        print(z)
+        return render_template("homepage.html",business= z)       
